eb50684d96bf96df70821d8ef110022b80dcb2cb
Raja, Mohaan <mohaan.raja@gmail.com>
2023-05-22 11:51:46 +0530

    Added Users tabl and  model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_14_043616) do
+ActiveRecord::Schema.define(version: 2023_05_22_061532) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2023_05_14_043616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
